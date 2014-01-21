3228bc6078c7c43572e45b0889bb8297842674bc
etiennefd <etienne.fortier-dubois@mail.mcgill.ca>
2014-01-20 19:59:39 -0500

    weapon skeleton and visible enum

--- Battlesheeps/src/battlesheeps/elements/Weapon.java
+++ Battlesheeps/src/battlesheeps/elements/Weapon.java
@@ -5,4 +5,8 @@ public class Weapon {
 	public enum WeaponType {
 		CANNON, MINE, TORPEDO 
 	}
+	
+	private WeaponType aType;		//Enum 
+	private int[] aRange; 			//Array of size two representing the dimension of the weapon's range
+	private boolean aHeavy; 		//true if the weapon can destroy heavy armour in one hit; false otherwise.
 }
--- Battlesheeps/src/battlesheeps/game/Game.java
+++ Battlesheeps/src/battlesheeps/game/Game.java
@@ -2,6 +2,8 @@ package battlesheeps.game;
 
 public class Game {
 
-
+	public enum Visible {
+		COVERED_BY_RONAR, COVERED_BY_SONAR, NOT_COVERED
+	}
 
 }
