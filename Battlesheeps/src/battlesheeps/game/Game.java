7654641b6dec94bada99792908a788e450332076
etiennefd <etienne.fortier-dubois@mail.mcgill.ca>
2014-02-01 14:27:09 -0500

    Started coding moves

--- Battlesheeps/src/battlesheeps/game/Game.java
+++ Battlesheeps/src/battlesheeps/game/Game.java
@@ -13,6 +13,11 @@ public class Game {
 		COVERED_BY_RADAR, COVERED_BY_SONAR, NOT_COVERED
 	}
 	
+	public enum MoveType {
+		TURN_SHIP, TRANSLATE_SHIP, FIRE_CANNON, FIRE_TORPEDO, 
+		DROP_MINE, PICKUP_MINE, TRIGGER_RADAR, REPAIR_SHIP
+	}
+	
 	private int aGameID;
 	private Account aPlayer1;
 	private Account aPlayer2;
@@ -23,6 +28,13 @@ public class Game {
 	private ArrayList<Ship> aShipListP1;
 	private ArrayList<Ship> aShipListP2;
 	
+	/**
+	 * Constructor for Games. Accepts an ID and the two players. 
+	 * Normally called from the GameManager based on the information held in the relevant GameRequest. 
+	 * @param pGameID
+	 * @param pPlayer1
+	 * @param pPlayer2
+	 */
 	public Game(int pGameID, Account pPlayer1, Account pPlayer2) {
 		
 		aGameID = pGameID;
@@ -44,6 +56,7 @@ public class Game {
 			}
 		}
 		
+		//Put random coral reefs on the board
 		generateCoralReefs();
 		
 		//Generating the ships for both players
@@ -74,7 +87,7 @@ public class Game {
 	}
 	
 	/**
-	 * Adds pNum coral reef squares to the board within the central 10*24 area. 
+	 * Adds 24 coral reef squares to the board within the central 10*24 area. 
 	 */
 	public void generateCoralReefs() {
 		for (int i = 0; i<24; i++) {
@@ -145,11 +158,71 @@ public class Game {
 		else {
 			System.out.println("Problem: coordinates don't match");//Should be an exception
 		}
-		
-		
 	}
 	
+	/**
+	 * Called from the GameManager to process a move on the board. 
+	 * A private method will be called depending on which kind of move it is. 
+	 * @param pShip
+	 * @param pMove
+	 */
+	public void computeMoveResult(Ship pShip, MoveType pMove, Coordinate pCoord) { //Removed GameID and player because the game calls this instead of the gameManager
+		switch (pMove) {
+		case TRANSLATE_SHIP: translateShip(pShip, pCoord); break;
+		case TURN_SHIP: turnShip(pShip, pCoord); break;
+		case FIRE_CANNON: fireCannon(pShip, pCoord); break;
+		case FIRE_TORPEDO: fireTorpedo(pShip, pCoord); break;
+		case DROP_MINE: dropMine(pShip, pCoord); break;
+		case PICKUP_MINE: pickupMine(pShip, pCoord); break;
+		case TRIGGER_RADAR: triggerRadar(pShip, pCoord); break;
+		case REPAIR_SHIP: repairShip(pShip, pCoord); break;
+		}
+	}
 	
+	private void translateShip(Ship pShip, Coordinate pCoord) {
+		/* TODO
+		 * Get the current location of the ship
+		 * Determine direction of translation (forward, backward, port, starboard)
+		 * If forward: 
+		 * 	determine if there are any obstacles (enemy ships or mine) between head of ship and goal point
+		 *  if yes, determine new position and place the ship there
+		 *  if it was a mine, call mineExplode()? (damage the ship and remove the mine)
+		 *  if there was no obstacle, assign the ship's position to a new position based on pCoord (represents the new position of the head)
+		 * If some other direction: 
+		 *  do the same, except that obstacles have to be checked for every square of the ship facing the direction
+		 * In all cases, recompute visibility
+		 */
+	}
+	private void turnShip(Ship pShip, Coordinate pCoord) {
+
+	}
+	private void fireCannon(Ship pShip, Coordinate pCoord) {
+
+	}
+	private void fireTorpedo(Ship pShip, Coordinate pCoord) {
+
+	}
+	private void dropMine(Ship pShip, Coordinate pCoord) {
+
+	}
+	private void pickupMine(Ship pShip, Coordinate pCoord) {
+
+	}
+	private void triggerRadar(Ship pShip, Coordinate pCoord) {
+
+	}
+	private void repairShip(Ship pShip, Coordinate pCoord) {
+
+	}
+	
+	private void mineExplode() {
+		
+	}
+
+	/**
+	 * Creates, as a string, an ASCII representation of the board. 
+	 * @return
+	 */
 	public String printBoard() {
 		String s = "Legend: \n" +
 				"~  - Empty sea square\n" +
