f6a05dcdfc07466c85ea5c76d4c5b33e7eb030be
Kate <kate.sprung@gmail.com>
2014-03-13 20:50:54 -0400

    kate's hosting now!

--- Battlesheeps/src/battlesheeps/networking/ClientGamesAndMoves.java
+++ Battlesheeps/src/battlesheeps/networking/ClientGamesAndMoves.java
@@ -21,7 +21,7 @@ public class ClientGamesAndMoves
     /* Host to connect to. This can be "localhost" if running both client/server 
      * on your computer, or the IP address of the host. 
      */
-    private static final String HOST = "localhost"; 
+    private static final String HOST = "142.157.145.143"; 
     private ObjectOutputStream aOutput = null;
 
 //    public static void main(String[] args)
