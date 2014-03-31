dc75f6b324e86742b724c3a496300bdd958fd48b
Kate <kate.sprung@gmail.com>
2014-03-30 22:06:18 -0400

    added new methods required to show the board

--- Battlesheeps/src/battlesheeps/test/TestClient.java
+++ Battlesheeps/src/battlesheeps/test/TestClient.java
@@ -6,7 +6,9 @@ import battlesheeps.networking.ClientGamesAndMoves;
 public class TestClient {
 	
 	public static void main(String[] args) {
-		ClientGame cl = new ClientGame("q");
-		ClientGamesAndMoves manager = new ClientGamesAndMoves("q", null, 1, cl); 
+		ClientGame cl = new ClientGame("a");
+		cl.addBoard("a");
+		cl.setupComplete();
+		ClientGamesAndMoves manager = new ClientGamesAndMoves("a", null, 1, cl); 
 	}
 }
